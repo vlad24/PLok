9d97dce43fd8ea3ce6b25dfeeff096376e44f982
Vladislav Pavlov <vlad.pavlov24@gmail.com>
2017-01-08 22:13:43 +0300

    CustomDistribution. Added toProbabilities. Not tested

--- src/main/java/ru/spbu/math/plok/model/client/distributions/CustomDistribution.java
+++ src/main/java/ru/spbu/math/plok/model/client/distributions/CustomDistribution.java
@@ -20,9 +20,10 @@ import ch.qos.logback.classic.Level;
 
 public class CustomDistribution extends Distribution{
 	private final static Logger log = LoggerFactory.getLogger(CustomDistribution.class);
-	private static final float SMALL_FLOAT = 0.0000001f;
-	private float[][] matrix;
-	
+	private static final float SMALL_FLOAT = 0.000001f;
+	private float[][] distribution;
+	private float[][] probabilites;
+
 	@Inject
 	public CustomDistribution(@Named("V") String v) throws IOException {
 		super(v);
@@ -30,33 +31,54 @@ public class CustomDistribution extends Distribution{
 		log.debug("Reading ;-separated matrix from {}", matrixFile.getAbsolutePath());
 		BufferedReader reader = new BufferedReader(new FileReader(matrixFile));
 		List<List<String>> fileData = getFileData(reader);
-		log.trace("File data got : {} ", fileData);
+		log.debug("File data got : {} ", fileData);
 		if (!fileData.isEmpty()){
-			matrix = new float[fileData.size()][];
-			toFloatDistributionMatrix(fileData);
+			distribution = buildDistributionMatrix(fileData);
+			probabilites = buildProbabilitiesMatrix(distribution);
 		}else{
 			throw new IllegalArgumentException("Empty file got!");
 		}
 		log.debug("Custom distribution successfully constructed");
+		log.debug("Distributions: {} ", getDistributionMatrix());
+		log.debug("Probabilities: {} ", getProbabilitiesMatrix());
 	}
 
-	private void toFloatDistributionMatrix(List<List<String>> fileData) {
+	private float[][] buildDistributionMatrix(List<List<String>> fileData) {
 		int rowAmount = fileData.size();
+		float[][] distributionMatrix = new float[rowAmount][rowAmount];
 		for (int i = 0; i < rowAmount; i++) {
 			List<String> row = fileData.get(i);
 			if (row.size() != rowAmount)
 				throw new IllegalArgumentException("Error at line " + i + " : row size != row amount");
-			matrix[i] = new float[rowAmount];
-			for (int j = 0; j < row.size(); j++){
-				matrix[i][j] = Float.parseFloat(row.get(j));
+			distributionMatrix[i] = new float[rowAmount];
+			for (int j = 0; j < rowAmount; j++){
+				distributionMatrix[i][j] = Float.parseFloat(row.get(j));
 			}
 		}
-		validateConsistency(matrix);
+		validateConsistency(distributionMatrix);
+		return distributionMatrix;
+	}
+	
+	private float[][] buildProbabilitiesMatrix(float[][] distributionMatrix){
+		for (int i = 0; i < distributionMatrix.length; i++){
+			for (int j = 0; j < distributionMatrix.length; j++){
+				if (i == 0 && j == 0){
+					probabilites[i][j] = distributionMatrix[i][j];  
+				}else if (i == 0){
+					probabilites[i][j] = distributionMatrix[i][j] - distributionMatrix[i][j - 1];
+				}else if (j == 0){
+					probabilites[i][j] = distributionMatrix[i][j] - distributionMatrix[i - 1][j];
+				}else{
+					probabilites[i][j] = distributionMatrix[i][j] - distributionMatrix[i - 1][j] - distributionMatrix[i][j - 1] +  distributionMatrix[i - 1][j - 1];  
+				}
+				if (Math.abs(probabilites[i][j]) < SMALL_FLOAT)
+					probabilites[i][j] = 0f;
+			}
+		}
+		return probabilites;
 	}
 
 	private void validateConsistency(float[][] distrMatrix) {
-		if (Math.abs(distrMatrix[0][0]) > SMALL_FLOAT)
-			throw new IllegalArgumentException("Zero-zero element is not equal to zero!");
 		if (Math.abs(1 - distrMatrix[distrMatrix.length - 1][distrMatrix.length - 1]) > SMALL_FLOAT)
 			throw new IllegalArgumentException("Last-last element is not equalt to one!");
 		for (int i = 0; i < distrMatrix.length - 1 ; i++){
@@ -67,8 +89,8 @@ public class CustomDistribution extends Distribution{
 					throw new IllegalArgumentException("Error at " + i + " " + j + " matrix element. Negative element.");
 			}
 		}
-		
-		
+
+
 	}
 
 	private List<List<String>> getFileData(BufferedReader reader) throws IOException {
@@ -93,8 +115,12 @@ public class CustomDistribution extends Distribution{
 		return 0;
 	}
 
-	public float[][] getMatrix() {
-		return matrix;
+	public float[][] getDistributionMatrix() {
+		return distribution;
+	}
+
+	public float[][] getProbabilitiesMatrix() {
+		return probabilites;
 	}
 
 }
