8a63a1f1d247dc14a639b0c50551f70033302bd8
Vladislav Pavlov <vlad.pavlov24@gmail.com>
2017-04-04 10:57:34 +0300

    Solver: added name-keys constants

--- src/main/java/ru/spbu/math/plok/solvers/Solver.java
+++ src/main/java/ru/spbu/math/plok/solvers/Solver.java
@@ -7,6 +7,19 @@ import com.google.inject.name.Named;
 
 public abstract class Solver {
 	
+	public static final String P_KEY       = "P";
+	public static final String L_KEY       = "L";
+	public static final String I_MIN_KEY    = "iMin";
+	public static final String I_MAX_KEY    = "iMax";
+	public static final String J_MIN_KEY    = "jMin";
+	public static final String J_MAX_KEY    = "jMax";
+	public static final String QUERIES_KEY = "queries";
+	public static final String I_POLICY_KEY = "iPolicy";
+	public static final String J_POLICY_KEY = "jPolicy";
+	public static final String I_POLICY_HR_RANGES_KEY = "iPolicy_HR_ranges";
+	public static final String J_POLICY_RT_WINDOW_KEY = "jPolicy_RT_window";
+	
+	
 	protected String H;
 	
 	public Solver(String historyFile){
