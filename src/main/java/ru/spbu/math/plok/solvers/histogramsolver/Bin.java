daf98e41caa1e3b2375e94191250e34223b23262
Vladislav Pavlov <vlad.pavlov24@gmail.com>
2017-04-04 14:13:06 +0300

    Bin: equals + hashCode added. ValueType migrated to Histogram class

--- src/main/java/ru/spbu/math/plok/solvers/histogramsolver/Bin.java
+++ src/main/java/ru/spbu/math/plok/solvers/histogramsolver/Bin.java
@@ -1,39 +1,70 @@
 package ru.spbu.math.plok.solvers.histogramsolver;
 
-import java.text.DecimalFormat;
-
 public class Bin {
 	
-	static enum ValueType{
-		PERCENTAGE, RAW
-	}
-	
-	private int value;
+	private double value;
 	private int id;
 	private Double left;
 	private Double right;
-	private ValueType valueType;
 	
 	@Override
 	public String toString() {
-		DecimalFormat format = new DecimalFormat("#.##");
 		return new StringBuilder()
 		.append("B[")
-		.append(format.format(left)).append(",").append(format.format(right))
+		.append(String.format("%.2f", left))
+		.append(",")
+		.append(String.format("%.2f", right))
 		.append("# ").append(value)
-		.append(" ").append((valueType == ValueType.PERCENTAGE)? "%" : "pcs")
 		.append("]")
 		.toString()
 		;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((left == null) ? 0 : left.hashCode());
+		result = prime * result + ((right == null) ? 0 : right.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(value);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bin other = (Bin) obj;
+		if (id != other.id)
+			return false;
+		if (left == null) {
+			if (other.left != null)
+				return false;
+		} else if (!left.equals(other.left))
+			return false;
+		if (right == null) {
+			if (other.right != null)
+				return false;
+		} else if (!right.equals(other.right))
+			return false;
+		if (Double.doubleToLongBits(value) != Double.doubleToLongBits(other.value))
+			return false;
+		return true;
+	}
+
 	public Bin(int id, double left, double right, int value) {
 		super();
 		this.id = id;
 		this.left = left;
 		this.right = right;
 		this.value = value;
-		this.valueType = ValueType.RAW;
 	}
 	
 	public int getId() {
@@ -52,7 +83,7 @@ public class Bin {
 		this.left = left;
 	}
 	
-	public int getValue() {
+	public double getValue() {
 		return value;
 	}
 	
@@ -64,16 +95,8 @@ public class Bin {
 		this.right = right;
 	}
 	
-	public void setValue(int occurences) {
+	public void setValue(double occurences) {
 		this.value = occurences;
-	}
-
-	public ValueType getValueType() {
-		return valueType;
-	}
-
-	public void setValueType(ValueType valueType) {
-		this.valueType = valueType;
 	}
 
 	public void incrementValue() {
