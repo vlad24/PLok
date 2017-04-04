e8246b0b9a953352cd0ac84035daa52a95ee4c01
Vladislav Pavlov <vlad.pavlov24@gmail.com>
2017-04-04 14:16:37 +0300

    Histogram: constants added. ValueType migrated here

--- src/main/java/ru/spbu/math/plok/solvers/histogramsolver/Histogram.java
+++ src/main/java/ru/spbu/math/plok/solvers/histogramsolver/Histogram.java
@@ -10,28 +10,34 @@ import java.util.TreeMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ru.spbu.math.plok.solvers.histogramsolver.Bin.ValueType;
-
 /**
  * @author vlad
  * Histogram helper class
  */
 class Histogram<K extends Number>{
+	private static final double SMALL_VALUE = 0.001;
+	private static final double DISCRETE_BIN_OFFSET = 0.5;
+	private static final double CONTINUOUS_BIN_OFFSET = SMALL_VALUE;
+
 	private final static Logger log = LoggerFactory.getLogger(Histogram.class);
+
+	static enum ValueType{
+		PERCENTAGE, RAW
+	}
+
 
 	private int binCount;
 	private ArrayList<Bin> bins;
 	private double binWidth;
 	private double binOffset;
-	
 	private DecimalFormat floatTrimmer = new DecimalFormat("#.###");
 	private boolean isDiscrete;
 	private double max;
 	private double min;
 	private String name;
 	private int observations;
-
-	private TreeMap<K, Integer> rawOccs;
+	private TreeMap<K,Double> rawOccs;
+	private ValueType valueType = ValueType.RAW;
 
 
 	public Histogram(String name, List<K> data, K min, K max) {
@@ -41,39 +47,41 @@ class Histogram<K extends Number>{
 		this.min          = min.doubleValue();
 		this.max          = max.doubleValue();
 		this.observations = data.size();
-		this.rawOccs      = new TreeMap<K, Integer>();
+		this.rawOccs      = new TreeMap<K, Double>();
 		this.bins         = new ArrayList<>();
 		log.debug("Constructing {} for data: {}", name, data);
 		buildRawHistogram(data);
 		buildEquiWidthBinHistogram(data);
-		normalizeToPercents();
+		log.debug("Raw histograms built");
 	}
 
 	private void buildEquiWidthBinHistogram(List<K> data) {
 		binOffset = 0;
 		if (isDiscrete){
-			binOffset = 0.5;
+			binOffset = DISCRETE_BIN_OFFSET;
 			int distincts = new HashSet<K>(data).size();
 			this.binWidth = Math.max(1, (max - min) / Math.ceil(Math.sqrt(2 + distincts)));
 			this.binCount = (int) Math.ceil((max - min + 1) / binWidth);
-			log.debug("Distinct values: {} ", distincts);
-			log.debug("Bin width:       {} ", binWidth);
-			log.debug("Bin count:       {} ", binCount);
+			log.debug("Distinct discrete values: {} ", distincts);
 		}else{
+			binOffset = CONTINUOUS_BIN_OFFSET;
 			this.binCount = (int) Math.sqrt(data.size()); 
-			this.binWidth = Math.ceil((this.max - this.min) / binCount);
+			this.binWidth = SMALL_VALUE + (this.max - this.min) / binCount;
 		}
+		log.debug("Min Max  :    {} {} ", min, max);
+		log.debug("Bin width:       {} ", binWidth);
+		log.debug("Bin count:       {} ", binCount);
 		for (int i = 0; i < binCount; i++){
 			double left  = -binOffset + min + i * binWidth;
 			double right = left + binWidth;
-			if (left > max)
-				break;
 			bins.add(new Bin(i, left, right, 0));
 			log.debug("{}th bin {} inited", i, bins.get(i));
 		}
 		for (K k : data){
 			int id = binify(k);
-			bins.get(id).incrementValue();
+			Bin targetBin = bins.get(id);
+			assert (targetBin.getLeft() <= k.doubleValue()) && (k.doubleValue() <= targetBin.getRight());
+			targetBin.incrementValue();
 		}
 		log.debug("All bins have been filled");
 	}
@@ -82,7 +90,7 @@ class Histogram<K extends Number>{
 		for (K k : data){
 			@SuppressWarnings("unchecked")
 			K key = (isDiscrete)?  k : (K)Double.valueOf(floatTrimmer.format(k.doubleValue()));
-			Integer oldValue = rawOccs.get(key);
+			Double oldValue = rawOccs.get(key);
 			rawOccs.put(key, (oldValue == null)? 1 : oldValue + 1);
 		}
 	}
@@ -107,10 +115,10 @@ class Histogram<K extends Number>{
 	//Raw data
 	
 	public Bin getMaxBin(){
-		int maxOcc = Integer.MIN_VALUE;
+		double maxOcc = Double.MIN_VALUE;
 		Bin maxBin = null;
 		for (Bin bin : bins){
-			if (bin.getValue() > maxOcc){
+			if (Double.compare(bin.getValue(), maxOcc) > 0){
 				maxOcc = bin.getValue();
 				maxBin = bin;
 			}
@@ -120,8 +128,8 @@ class Histogram<K extends Number>{
 	
 	public K getMaxCountRaw(){
 		K result = null;
-		int maxOccurence = Integer.MIN_VALUE;
-		for (Map.Entry<K, Integer> entry : rawOccs.entrySet()){
+		double maxOccurence = Double.MIN_VALUE;
+		for (Map.Entry<K, Double> entry : rawOccs.entrySet()){
 			if (entry.getValue() >= maxOccurence){
 				maxOccurence = entry.getValue();
 				result = entry.getKey();
@@ -130,35 +138,25 @@ class Histogram<K extends Number>{
 		return result;
 	}
 	
-	public Integer getRawCount(K key){
-		Integer occurence = rawOccs.get(key);
+	public double getRawValue(K key){
+		Double occurence = rawOccs.get(key);
 		return occurence == null ? 0 : occurence;
 	}
 
-	private void normalizeToPercents() {
-		log.debug("Normalizing histograms...");
-		int percentage = 0;
+	public void normalizeToPercents() {
+		log.debug("Normalizing {} to percents");
+		double percentage = 0;
 		for (K k : rawOccs.keySet()){
-			percentage = (int)((100.0 * rawOccs.get(k)) / observations);
+			percentage = (100.0 * rawOccs.get(k)) / observations;
 			rawOccs.put(k, percentage);
 		}
-		int maxBinId      = -1;
-		int maxPercentage = -1;
-		int percentageSum = 0;
+		log.debug("Raw representation of {} normalized");
 		for (Bin bin : bins){
-			percentage = (int)((100.0 * bin.getValue()) / observations);
-			bin.setValueType(ValueType.PERCENTAGE);
+			percentage = (100.0 * bin.getValue()) / observations;
 			bin.setValue(percentage);
-			if (percentage > maxPercentage){
-				maxPercentage = percentage;
-				maxBinId = bin.getId();
-			}
-			percentageSum += percentage;
 		}
-		if (percentageSum < 100){
-			assert (percentageSum == 99);
-			getBin(maxBinId).incrementValue();
-		}
+		log.debug("Binned representation of {} normalized");
+		this.valueType = ValueType.PERCENTAGE;
 	}
 	
 	private int binify(K k) {
@@ -184,8 +182,10 @@ class Histogram<K extends Number>{
 		.append(isDiscrete? "{discrete}" : "{continuous}")
 		.append("\n");
 		for (Bin bin : bins){
-			result.append(bin.toString()).append(":\t");
-			for (int j = 0; j < bin.getValue(); j++){
+			result.append(bin.toString())
+			.append(valueType == ValueType.PERCENTAGE ? "%" : "pcs")
+			.append(":\t\t|");
+			for (int j = 1; j <= bin.getValue(); j++){
 				result.append("=");
 			}
 			result.append("\n");
@@ -198,9 +198,12 @@ class Histogram<K extends Number>{
 		result.append("\n").append(name).append(" ").append("[RAW]")
 		.append(isDiscrete? "{discrete}" : "{continuous}")
 		.append("\n");
-		for (Map.Entry<K, Integer> entry : rawOccs.entrySet()){
-			result.append(entry.getKey()).append(":\t");
-			for (int i = 0; i < entry.getValue(); i++){
+		for (Map.Entry<K, Double> entry : rawOccs.entrySet()){
+			result.append(entry.getKey())
+			.append("[").append(entry.getValue()).append("]")
+			.append(valueType == ValueType.PERCENTAGE ? "%" : "pcs")
+			.append(":\t\t|");
+			for (int i = 1; i <= entry.getValue(); i++){
 				result.append("=");
 			}
 			result.append("\n");
