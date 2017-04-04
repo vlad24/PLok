d7e40fbac976261128030a4264c15835dbd27f63
Vladislav Pavlov <vlad.pavlov24@gmail.com>
2017-04-04 10:57:56 +0300

    UserChoice: CAPITALIZING names

--- src/main/java/ru/spbu/math/plok/solvers/histogramsolver/UserChoice.java
+++ src/main/java/ru/spbu/math/plok/solvers/histogramsolver/UserChoice.java
@@ -2,8 +2,8 @@ package ru.spbu.math.plok.solvers.histogramsolver;
 
 public class UserChoice {
 	public static enum Policy{
-		RangeInterest,
-		FullTrack,
-		LateTracking
+		HOT_RANGES,
+		FULL_TRACK,
+		RECENT_TRACKING
 	}
 }
