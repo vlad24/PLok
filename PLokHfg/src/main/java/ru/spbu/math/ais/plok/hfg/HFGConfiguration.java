8361080e7daa650ecab0485a4d3052c43225e619
Vladislav Pavlov <vlad.pavlov24@gmail.com>
2017-05-02 20:57:08 +0300

    HFG refactored

---
+++ PLokHfg/src/main/java/ru/spbu/math/ais/plok/hfg/HFGConfiguration.java
@@ -0,0 +1,144 @@
+package ru.spbu.math.ais.plok.hfg;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.MoreObjects;
+
+import ru.spbu.math.ais.plok.MapKeyNames;
+import ru.spbu.math.ais.plok.solvers.histogramsolver.UserChoice.Policy;
+
+/**
+ * @author vlad
+ * iPolicy, jPolicy, history file, timeStep, count, vectorSize, verbosity, policiessParams
+ * 
+ */
+public class HFGConfiguration{
+
+	private static Logger log = LoggerFactory.getLogger(HFGConfiguration.class);
+	
+	private boolean inited;
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////// COMMON OPTS
+	@Option(name="-O",usage="History file", required=true)
+	private String 	historyFile;
+	
+	@Option(name="--iPolicy", usage="i policy", required=true)
+	private Policy iPolicy;
+	
+	@Option(name="--jPolicy", usage="j policy", required=true)
+	private Policy jPolicy;
+	
+	@Option(name="--timeStep",usage="read query avg period", required=true)
+	private Integer timeStep;
+	
+	@Option(name="--vectorSize",usage="vector size(N)", required=true)
+	private Integer vectorSize;
+	
+	@Option(name="--count",usage="amount of queries", required=true)
+	private Integer queriesCount;
+
+	@Option(name="--verbosity",usage="Verbosity level", required=false)
+	private String  verbosity = "info";
+	
+	@Option(name="--windowSize",usage="Window size for JPolicy.RecentTracking", required=false)
+	private Long    windowSize;
+	
+	@Option(name="--hotRanges",usage="Hot ranges for IPolicy.HotRanges", required=false)
+	private String	hotRanges;
+	
+	
+	public HFGConfiguration(){
+		inited = false;
+	}
+
+	public HFGConfiguration(String[] args){
+		super();
+		initConfigs(args);
+	}
+
+	private void initConfigs(String[] args){
+		if (!inited){
+			CmdLineParser parser = new CmdLineParser(this);
+			try {
+				parser.parseArgument(args);
+				inited = true;
+			} catch (CmdLineException e) {
+				log.error(e.toString());
+			}
+		}
+	}
+
+	
+
+	@Override
+	public String toString() {
+		return MoreObjects.toStringHelper("")
+				.omitNullValues()
+				.add("O",     historyFile)
+				.add("N",     vectorSize)
+				.add("Q",     queriesCount)
+				.add("R",     timeStep)
+				.add("W",     windowSize)
+				.toString()
+				.replace("{", "").replace("}", "");
+	}
+	
+	/*
+	 * Getters and setters go below
+	 */
+
+	public boolean isInited() {
+		return inited;
+	}
+
+	public String getHistoryFile() {
+		return historyFile;
+	}
+
+	public Policy getiPolicy() {
+		return iPolicy;
+	}
+
+	public Policy getjPolicy() {
+		return jPolicy;
+	}
+
+	public Integer getTimeStep() {
+		return timeStep;
+	}
+
+	public Integer getVectorSize() {
+		return vectorSize;
+	}
+
+	public Integer getQueriesCount() {
+		return queriesCount;
+	}
+
+	public String getVerbosity() {
+		return verbosity;
+	}
+
+	public Long getWindowSize() {
+		return windowSize;
+	}
+
+	public Map<String, Object> getPoliciesParams() {
+		LinkedHashMap<String, Object> params = new LinkedHashMap<>();
+		if (hotRanges != null){	
+			params.put(MapKeyNames.I_POLICY_HR_RANGES_KEY, hotRanges);
+		}
+		if (windowSize != null){	
+			params.put(MapKeyNames.J_POLICY_RT_WINDOW_KEY, windowSize);
+		}
+		return params;
+	}
+	
+}
--- PLokHfg/src/main/java/ru/spbu/math/ais/plok/hfg/HistoryFileGenerator.java
+++ PLokHfg/src/main/java/ru/spbu/math/ais/plok/hfg/HistoryFileGenerator.java
@@ -1,89 +1,59 @@
 package ru.spbu.math.ais.plok.hfg;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
-import ru.spbu.math.ais.plok.MapKeyNames;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
 import ru.spbu.math.ais.plok.bench.QueryGenerator;
 import ru.spbu.math.ais.plok.model.client.Query;
 import ru.spbu.math.ais.plok.solvers.HistoryPreprocessor;
-import ru.spbu.math.ais.plok.solvers.histogramsolver.UserChoice.Policy;
 
 public class HistoryFileGenerator{
-
-
+	
+	private final static org.slf4j.Logger log = LoggerFactory.getLogger(QueryGenerator.class);
+	
 	/**
 	 * Creates dataSet file.
-	 * @param args [iPolicy, jPolicy, N, timeStep, count, fileName, policyParamsSlashedString]
-	 * @throws Exception
+	 * @see HFGConfiguration
 	 */
 	public static void main(String[] args) throws Exception{
-		if (args.length >= 4){
-			Policy iPolicy      = Policy.valueOf(args[0]);
-			Policy jPolicy      = Policy.valueOf(args[1]);
-			int vectorLength    = Integer.parseInt(args[2]);
-			int timeStep        = Integer.parseInt(args[3]);
-			int queriesCount    = Integer.parseInt(args[4]);
-			String fileName     = args[5];
-			String paramsString = null;
-			if (args.length >= 6){
-				paramsString = args[6];
-			}
-			File output       = new File(fileName);
-			FileWriter writer = new FileWriter(output);
+		HFGConfiguration config = new HFGConfiguration(args);
+		((ch.qos.logback.classic.Logger)LoggerFactory.getLogger("ROOT")).setLevel(Level.valueOf(config.getVerbosity()));
+		File output       = new File(config.getHistoryFile());
+		BufferedWriter writer = new BufferedWriter(new FileWriter(output));
+		writer.write(
+				new StringBuilder()
+				.append("#Created by HistoryFileGenerator at ").append(new Date().toString()).append("\n")
+				.append("#Parameters: ").append(config.toString())
+				.toString()
+				);
+		writer.newLine();
+		writer.write("@" + config.getVectorSize());
+		writer.newLine();
+		QueryGenerator queryGenerator = new QueryGenerator(config.getVectorSize(), config.getiPolicy(),
+				config.getjPolicy(), config.getPoliciesParams());
+		long time = 0;
+		for (int i = 0; i < config.getQueriesCount(); i++){
+			time += config.getTimeStep();
+			Query query = queryGenerator.nextQuery(time);
 			writer.write(
-					new StringBuilder()
-					.append("#Created by HistoryFileGenerator at ").append(new Date().toString()).append("\n")
-					.append("#Parameters: ").append(Arrays.toString(args)).append("\n")
-					.toString()
-					);
-			HashMap<String, Object> qGenParams = packParams(vectorLength, iPolicy, jPolicy, paramsString);
-			writer.write("@" + qGenParams.get(MapKeyNames.N_KEY) + "\n");
-			QueryGenerator queryGenerator = new QueryGenerator(vectorLength, iPolicy, jPolicy, (Map<String, Object>) qGenParams.get(MapKeyNames.POLICIES_PARAMS));
-			long t = 0;
-			for (int i = 0; i <= queriesCount; i++){
-				t += timeStep;
-				Query q = queryGenerator.nextQuery(t);
-				writer.write(
 						new StringBuilder()
-						.append(q.getTime()).append(HistoryPreprocessor.ELEMENT_SEPARATOR)
-						.append(q.getI1())  .append(HistoryPreprocessor.ELEMENT_SEPARATOR)
-						.append(q.getI2())  .append(HistoryPreprocessor.ELEMENT_SEPARATOR)
-						.append(q.getJ1())  .append(HistoryPreprocessor.ELEMENT_SEPARATOR)
-						.append(q.getJ2())
-						.append("\n")
+						.append(query.getTime()).append(HistoryPreprocessor.ELEMENT_SEPARATOR)
+						.append(query.getI1())  .append(HistoryPreprocessor.ELEMENT_SEPARATOR)
+						.append(query.getI2())  .append(HistoryPreprocessor.ELEMENT_SEPARATOR)
+						.append(query.getJ1())  .append(HistoryPreprocessor.ELEMENT_SEPARATOR)
+						.append(query.getJ2())
 						.toString()
-						);
-			}
-			writer.flush();
-			writer.close();
-			System.out.println("Done generating file " + fileName);
-		}else{
-			throw new Exception("No output file specified!");
+					);
+			writer.newLine();
 		}
+		writer.flush();
+		writer.close();
+		log.info("Done generating file {}", config.getHistoryFile());
 	}
 
-	private static HashMap<String, Object> packParams(int N, Policy iPolicy, Policy jPolicy, String paramsString) {
-		HashMap<String, Object> queryGeneratorParams = new HashMap<>();
-		queryGeneratorParams.put(MapKeyNames.N_KEY, N);
-		queryGeneratorParams.put(MapKeyNames.I_POLICY_KEY, iPolicy);
-		queryGeneratorParams.put(MapKeyNames.J_POLICY_KEY, jPolicy);
-		Map<String, Object> policiesParams = new HashMap<>(); 
-		if (paramsString != null){
-			String[] params = paramsString.split(HistoryPreprocessor.HINTS_SEPARATOR);
-			int currentParam = 0;
-			if (iPolicy == Policy.HOT_RANGES){
-				policiesParams.put(MapKeyNames.I_POLICY_HR_RANGES_KEY, params[currentParam++]);
-			}
-			if (jPolicy == Policy.RECENT_TRACKING){
-				policiesParams.put(MapKeyNames.J_POLICY_RT_WINDOW_KEY, Long.valueOf(params[currentParam++]));
-			}
-			queryGeneratorParams.put(MapKeyNames.POLICIES_PARAMS, policiesParams);
-		}
-		return queryGeneratorParams;
-	}
 
 }
